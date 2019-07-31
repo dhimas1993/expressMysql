9aec50b1786f85ec4fca1c99d53e14d2df091921
dhimas hertianto <dhimashertianto@dhimass-MacBook-Pro.local>
2019-07-31 10:55:07 +0700

    add home route

--- src/index.js
+++ src/index.js
@@ -12,6 +12,11 @@ app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
+app.get('/', (req,res) => {
+    res.send(`
+    <h1>halo bisa nih</h1>`)
+})
+
 app.listen(port, () => {
     console.log('Berhasil Running di ' + port);
     
